0a16d237d0845f8ee63622d5ee911d55161c9e5e
MelkerUlmstedt <melker.ulmstedt2@elev.ga.ntig.se>
2024-04-11 22:51:30 +0200

    Första deadline
    Fixat allt crud och inlogg. Får inte inner join att fungera dock vilket är frustrerande menmen

--- app.rb
+++ app.rb
@@ -6,15 +6,63 @@ require 'sinatra/reloader'
 
 enable :sessions
 
-get('/')  do
-    slim(:start)
+  
+get('/') do
+    slim(:login)
 end 
+
+get('/register') do
+    slim(:register)
+end
+
+get('/main') do
+    id = session[:id].to_i
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM User WHERE user_id = ?",id).first
+    slim(:start,locals:{result:result})
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM User WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["user_id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/main')
+    else
+      "FEL LÖSENORD"
+    end
+end
+
+post('/users/new') do
+    username = params[:username]
+    password_confirm = params[:password_confirm]
+    password = params[:password]
+  
+    if (password_confirm == password)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/database.db')
+      db.execute("INSERT INTO User (username,pwdigest) VALUES (?,?)",username,password_digest)
+      redirect('/')
+    else
+       "Lösenorden matcher ej"
+    end
+end
+
+
 
 #Schedules
 get('/schedules') do
+    id = session[:id].to_i
     db = SQLite3::Database.new("db/database.db")
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM Schedule")
+    result = db.execute("SELECT * FROM Schedule WHERE user_id = ?",id)
     slim(:"schedules/index",locals:{schedules:result})
 end
 
@@ -24,8 +72,11 @@ end
 
 post('/schedules/new') do
     name = params[:name]
+    user_id = session[:id].to_i
+    p name
+    p user_id
     db = SQLite3::Database.new("db/database.db")
-    db.execute("INSERT INTO Schedule (name) VALUES (?)",name)
+    db.execute("INSERT INTO Schedule (name,user_id) VALUES (?,?)",name,user_id)
     redirect('/schedules')
 end
 
@@ -77,12 +128,12 @@ get('/exercises/new') do
 end
 
 post('/exercises/new') do
-    exercise = params[:exercise]
-    primary_muscle_id = params[:primary_muscle_id].to_i- #Får inte vara noll, ta reda på hur man gör detta
-    secondary_muscle_id = params[:secondary_muscle_id].to_i
-    third_muscle_id = params[:third_muscle_id].to_i
+    exercise_name = params[:exercise_name]
+    muscle1_id = params[:muscle1_id].to_i #Skapa felhantering om man försöker sätta id = 0
+    muscle2_id = params[:muscle2_id].to_i
+    muscle3_id = params[:muscle3_id].to_i
     db = SQLite3::Database.new("db/database.db")
-    db.execute("INSERT INTO Exercises (exercise,primary_muscle_id,secondary_muscle_id,third_muscle_id) VALUES (?,?,?,?)",exercise,primary_muscle_id,secondary_muscle_id,third_muscle_id)
+    db.execute("INSERT INTO Exercises (exercise_name,muscle1_id,muscle2_id,muscle3_id) VALUES (?,?,?,?)",exercise_name,muscle1_id,muscle2_id,muscle3_id)
     redirect('/exercises')
 end
 
@@ -95,12 +146,12 @@ end
 
 post('/exercises/:id/update') do
     exercise_id = params[:id].to_i
-    exercise = params[:exercise]
-    primary_muscle_id = params[:primary_muscle_id].to_i
-    secondary_muscle_id = params[:secondary_muscle_id].to_i
-    third_muscle_id = params[:third_muscle_id].to_i
+    exercise_name = params[:exercise_name]
+    muscle1_id = params[:muscle1_id].to_i
+    muscle2_id = params[:muscle2_id].to_i
+    muscle3_id = params[:muscle3_id].to_i
     db = SQLite3::Database.new("db/database.db")
-    db.execute("UPDATE Exercises SET exercise=?,primary_muscle_id=?,secondary_muscle_id=?,third_muscle_id=? WHERE exercise_id = ?", exercise,primary_muscle_id,secondary_muscle_id,third_muscle_id,exercise_id)
+    db.execute("UPDATE Exercises SET exercise_name=?,muscle1_id=?,muscle2_id=?,muscle3_id=? WHERE exercise_id = ?", exercise_name,muscle1_id,muscle2_id,muscle3_id,exercise_id)
     redirect('/exercises')
 end
 
@@ -117,8 +168,10 @@ get('/exercises/:id') do
     db = SQLite3::Database.new("db/database.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM Exercises WHERE exercise_id = ?",exercise_id).first
-    #result2 = db.execute("SELECT Name FROM MuscleTypes WHERE ArtistId IN (SELECT ArtistId FROM Exercises WHERE id = ?)", exercise_id).first
-    #result3 = db.execute("SELECT Exercises.primary_muscle_id, Exercises.secondary_muscle_id, Exercises.third_muscle_id, FROM Exercises INNER JOIN MuscleTypes ON Exercises.id = ?",id)
-    slim(:"exercises/show",locals:{result:result})
+    result2 = db.execute("SELECT Name FROM MuscleTypes INNER JOIN Exercises ON Exercises.muscle1_id = MuscleTypes.id").first
+    result3 = db.execute("SELECT Name FROM MuscleTypes INNER JOIN Exercises ON Exercises.muscle2_id = MuscleTypes.id").first
+    result4 = db.execute("SELECT Name FROM MuscleTypes INNER JOIN Exercises ON Exercises.muscle3_id = MuscleTypes.id").first
+    #result3 = db.execute("SELECT Exercises.muscle1_id, Exercises.secondary_muscle_id, Exercises.third_muscle_id, FROM Exercises INNER JOIN MuscleTypes ON Exercises.id = ?",id)
+    slim(:"exercises/show",locals:{result:result, result2:result2, result3:result3, result4:result4})
 end
     
